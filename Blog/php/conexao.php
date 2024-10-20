2dfae0a18f0b64293bec573013dcfdfde593495b
Izabely Ferrari <belymiaferrari@gmail.com>
2024-10-20 19:25:11 -0300

    Adicionando PHP

---
+++ Blog/php/conexao.php
@@ -0,0 +1,15 @@
+<?php 
+$host = "localhost";
+$usuario = "root";
+$senha = "usbw"; 
+$bd = "login";
+
+$mysqli = new mysqli($host, $usuario, $senha , $bd);
+if ($mysqli -> connect_errno){
+    echo "Falha ao conectar: (" . $mysqli -> connect_errno . ")" . $mysqli -> connect_error;
+
+
+}
+
+
+?>
