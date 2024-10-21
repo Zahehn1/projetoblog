be110e770a5174054b7a8629425ed5595e87f210
Samuel Cabral <samuelcabral590@gmail.com>
2024-10-20 23:10:28 -0300

    Update index.php

--- Blog/php/index.php
+++ Blog/php/index.php
@@ -30,29 +30,20 @@ if(isset($_POST['email'])|| isset($_POST['SENHA'])) {
 }
 ?>
 
-
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Projeto blog | Login</title>
 </head>
 <body>
-    <h1>Acesse sua conta!</h1>
-    <form action="" method="POST">
-    <p>
-     <label>E-mail</label>
-     <input type="text" name="email" >
-    </p>
-    <p>
-     <label>Senha</label>
-     <input type="password" name="senha" >
-    </p>
-    <p>
-       <button type = "submite">Entrar</button>
-
-    </p>
-    </form>
+    <main>
+        <section>
+            <legend id="loginForm">
+        </legend>
+        </section>
+    </main>
 </body>
+<script src="../html/JS/loginForm.js"></script>
 </html>
