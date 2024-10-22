a9e4905fccb403e51401d999419af4aa730d22e9
Samuel Cabral <samuelcabral590@gmail.com>
2024-10-21 23:29:56 -0300

    Feito a criação da página 'templates', tratado mensagem de erro ao realizar login, adicionado css a pagina de registro

--- Blog/php/index.php
+++ Blog/php/index.php
@@ -3,12 +3,13 @@ session_start();
 include('conexao.php');
 
 $email_input = ''; 
+$error_message = '';
 
 if (isset($_POST['email']) || isset($_POST['senha'])) {
     if (strlen($_POST['email']) == 0) {
-        echo "Preencha seu E-mail";
+        $error_message = "Preencha seu E-mail";
     } else if (strlen($_POST['senha']) == 0) {
-        echo "Preencha sua senha";
+        $error_message = "Preencha sua senha";
     } else {
         $email = $mysqli->real_escape_string($_POST['email']);
         $senha = $mysqli->real_escape_string($_POST['senha']);
@@ -30,12 +31,11 @@ if (isset($_POST['email']) || isset($_POST['senha'])) {
             header("Location: ../html/inicio.html");
             exit();
         } else {
-            echo "Falha ao logar! E-mail ou senha incorretos";
+            $error_message = "Falha ao logar! E-mail ou senha incorretos";
             $email_input = $_POST['email']; 
         }
     }
 }
-?>
 
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -49,15 +49,14 @@ if (isset($_POST['email']) || isset($_POST['senha'])) {
 <body>
     <main>
         <section>
-         <h2>Faça seu login</h2>
+            <h2>Faça seu login</h2>
             <legend>
-             <div class="error-message">
-               <?php if (!empty($error_message)) { echo $error_message; } ?>
-             </div>
+                <div class="error-message">
+                    <?php if (!empty($error_message)) { echo $error_message; } ?>
+                </div>
                 <form method="POST" action="">
                     <div class="input-container">
                         <input id="email" name="email" type="email" placeholder=" " required value="<?php echo htmlspecialchars($email_input); ?>">
-
                         <label for="email">Digite seu e-mail</label>
                     </div>
                     
