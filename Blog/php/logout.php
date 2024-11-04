09270d07f84ba73e068d20d665cd5240e922d37a
Zahehn1 <samuelcabral590@gmail.com>
2024-11-04 08:26:03 -0300

    criação do botao de logout.

--- Blog/php/logout.php
+++ Blog/php/logout.php
@@ -1,8 +1,9 @@
 <?php 
-session_start();
+session_start(); 
 
-if(isset($_SESSION)){
-  session_start();
-}
-session_destroy();
+session_unset();
+session_destroy(); 
+
 header("Location: index.php");
+exit();
+?>
